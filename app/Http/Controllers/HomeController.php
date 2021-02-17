60f206ba71123fe31eff88930b3d35d235d22b5f
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2021-02-17 17:34:51 +0100

    Creati file

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function home()
+    {
+    return view('home');
+    }
+
+    public function about()
+    {
+    return view('about');
+    }
+
+    public function blog(Post $post)
+    {
+
+    $posts = $post->all();
+
+    dd($posts);
+
+    return view('blog');
+    }
+
+}
+ 
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,6 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    Ciao
+    
 </body>
 </html>
